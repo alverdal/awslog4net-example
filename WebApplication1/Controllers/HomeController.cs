1f21c0c67d051bdfcacafac5b587f26449c59cb6
Johan Älverdal <johan.alverdal@zmartagroup.com>
2022-01-07 11:41:42 +0100

    initial commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static ILog _log = LogManager.GetLogger("homeController");
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            _log.Info("test");
+
+            return View();
+        }
+    }
+}
